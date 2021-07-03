1e139a67e6cec27ecc233a10fb53cb960824626c
Nicole <nicole_vasquez_Silva@hotmail.com>
2021-07-02 23:47:58 -0500

    fixes-test

--- assets/js/main.js
+++ assets/js/main.js
@@ -188,10 +188,10 @@
      * Testimonials slider
      */
     new Swiper('.testimonials-slider', {
-        speed: 600,
+        speed: 2000,
         loop: true,
         autoplay: {
-            delay: 5000,
+            delay: 7000,
             disableOnInteraction: false
         },
         slidesPerView: 'auto',
